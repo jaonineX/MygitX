5c5ac3aa3827fcdeef82946879f80feb771de7ca
jaonineX <156872630+jaonineX@users.noreply.github.com>
2024-01-24 11:57:34 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -48,14 +48,14 @@ x2=df['sepal.length'].mean()
 x3=df['petal.width'].mean()
 x4=df['petal.length'].mean()
 
-    sizes = [x1, x2, x3, x4]
+sizes = [x1, x2, x3, x4]
 
-    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
             shadow=True, startangle=90)
-    #ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ #ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     
 
