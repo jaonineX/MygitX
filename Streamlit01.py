c9581d38ae3b9d4707d65de66b0c1a4fcf51d8cd
jaonineX <156872630+jaonineX@users.noreply.github.com>
2024-01-24 11:31:01 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -56,8 +56,7 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 st.pyplot(fig1)
 
 if(st.button("แสดงกราฟวงกลม")):
-    # Pie 
-labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
+{labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
 x1=df['sepal.width'].mean()
 x2=df['sepal.length'].mean()
 x3=df['petal.width'].mean()
@@ -69,7 +68,7 @@ fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 #ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
+}
 st.pyplot(fig1)
 
  st.button("ไม่แสดงกราฟวงกลม")
